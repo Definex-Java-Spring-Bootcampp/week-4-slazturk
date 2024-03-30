eab31e62f7df6342872d9bdc0c17b11fc18da592
cemdrman <cemdrman@gmail.com>
2024-03-30 14:00:56 +0300

    db connection

--- src/main/java/com/patika/kredinbizdeservice/model/User.java
+++ src/main/java/com/patika/kredinbizdeservice/model/User.java
@@ -1,5 +1,12 @@
 package com.patika.kredinbizdeservice.model;
 
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
@@ -7,100 +14,22 @@ import java.time.LocalDate;
 import java.util.List;
 
 @ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
+    @Id
+    private Long id;
     private String name;
     private String surname;
-   // private LocalDate birthDate;
+    private LocalDate birthDate;
     private String email; //bir email ile bir kere kayıt olunabilir.
     private String password; //hash fonskiyonlarından biri ile hashlanecek.
     private String phoneNumber;
     private Boolean isActive;
     //private List<Application> applicationList;
 
-    public User() {
-    }
-
-    public User(String name, String surname, LocalDate birthDate, String email, String password, String phoneNumber, Boolean isActive) {
-        this.name = name;
-        this.surname = surname;
-      ///  this.birthDate = birthDate;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.isActive = isActive;
-    }
-
-    public User(String name, String surname, String email, String password, String phoneNumber, Boolean isActive) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.isActive = isActive;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-/*
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public List<Application> getApplicationList() {
-        return applicationList;
-    }
-
-    public void setApplicationList(List<Application> applicationList) {
-        this.applicationList = applicationList;
-    }*/
 }
