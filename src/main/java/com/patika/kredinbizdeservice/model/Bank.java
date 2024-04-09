71485cbe93043222a8a3e33c6a9dc2db3c733bc3
Yaprak Sila Ozturk <yapraksila1@gmail.com>
2024-04-09 21:01:20 +0300

    first commit

--- src/main/java/com/patika/kredinbizdeservice/model/Bank.java
+++ src/main/java/com/patika/kredinbizdeservice/model/Bank.java
@@ -2,33 +2,43 @@ package com.patika.kredinbizdeservice.model;
 
 import java.util.List;
 
+import com.patika.kredinbizdeservice.model.constant.BankEntityColumnConstants;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "application")
+
 public class Bank {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column(name = BankEntityColumnConstants.NAME, unique = false, nullable = false)
     private String name;
+	
+	@OneToMany(mappedBy = "loan", cascade = CascadeType.ALL)
     private List<Loan> loanList;
+	
+	@OneToMany(mappedBy = "credit", cascade = CascadeType.ALL)
     private List<CreditCard> creditCards;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Loan> getLoanList() {
-        return loanList;
-    }
-
-    public void setLoanList(List<Loan> loanList) {
-        this.loanList = loanList;
-    }
-
-    @Override
-    public String toString() {
-        return "Bank{" +
-                "name='" + name + '\'' +
-                ", loanList=" + loanList +
-                '}';
-    }
 }
--- src/main/java/com/patika/kredinbizdeservice/model/Loan.java
+++ src/main/java/com/patika/kredinbizdeservice/model/Loan.java
@@ -2,66 +2,50 @@ package com.patika.kredinbizdeservice.model;
 
 import java.math.BigDecimal;
 
+import com.patika.kredinbizdeservice.model.constant.LoanEntityColumnConstants;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "loan")
+
 public abstract class Loan implements Product {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column(name = LoanEntityColumnConstants.AMOUNT, unique = false, nullable = false)
     private BigDecimal amount;
+	
+	@Column(name = LoanEntityColumnConstants.INSTALLMENT, unique = false, nullable = false)
     private Integer installment;
+	
+	@ManyToOne
+    @JoinColumn(name = "bank_id")
     private Bank bank;
+	
+	@Column(name = LoanEntityColumnConstants.INTEREST_RATE, unique = false, nullable = false)
     private Double interestRate;
     // private Campaign campaign; // kampanyalı kredileri üstte çıkart
 
     //sponsorlu kampanyaları üstte çıkart
 
-    public Loan() {
-    }
-
-    public Loan(BigDecimal amount, Integer installment, Double interestRate) {
-        this.amount = amount;
-        this.installment = installment;
-        this.interestRate = interestRate;
-    }
-
-    abstract void calculate(BigDecimal amount, int installment);
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Integer getInstallment() {
-        return installment;
-    }
-
-    public void setInstallment(Integer installment) {
-        this.installment = installment;
-    }
-
-    public Bank getBank() {
-        return bank;
-    }
-
-    public void setBank(Bank bank) {
-        this.bank = bank;
-    }
-
-    public Double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
-
-    @Override
-    public String toString() {
-        return "Loan{" +
-                "amount=" + amount +
-                ", installment=" + installment +
-                ", bank=" + bank +
-                ", interestRate=" + interestRate +
-                '}';
-    }
+   
 }
