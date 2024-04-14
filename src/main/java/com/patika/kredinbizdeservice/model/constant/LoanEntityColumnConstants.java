f9792268484878d9dbcbf270239c4e6a21bc0ea4
Yaprak Sıla Öztürk <106006892+slazturk@users.noreply.github.com>
2024-04-14 23:43:26 +0300

    Delete src directory

--- src/main/java/com/patika/kredinbizdeservice/model/constant/LoanEntityColumnConstants.java
+++
@@ -1,9 +0,0 @@
-package com.patika.kredinbizdeservice.model.constant;
-
-public class LoanEntityColumnConstants {
-	
-	public static final String AMOUNT = "amount";
-    public static final String INSTALLMENT = "installment";
-    public static final String INTEREST_RATE = "interest_rate";
-   
-}
